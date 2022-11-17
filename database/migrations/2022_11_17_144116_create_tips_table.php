96ee5b363ef5198795c2384964ec36d4b9bcbde6
rizky ferdian <rizkyferdian002@gmail.com>
2022-11-17 22:28:36 +0700

    add migrations

---
+++ database/migrations/2022_11_17_144116_create_tips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tips', function (Blueprint $table) {
+            $table->id();
+            $table->String('title');
+            $table->String('url');
+            $table->String('thumbnail');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tips');
+    }
+}
